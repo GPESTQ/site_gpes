1f45f5e1370904b1b95ab09c477fdf10a321af65
Otávio Sudano <otavio.sudano13@gmail.com>
2026-08-31 17:58:11 -0300

    fix: correção no use de * na rota de produção

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,12 +34,12 @@ app.use("/api/papers", papersRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get('(.*)', (_, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, _) => {
     console.error("Unhandled error:", err);
     res.status(500).json({ message: "Internal server error" });
 });
